ed4079a719d285bc5e53a09300c3dacdae9f878e
allison-higgins@bts.k12.nj.us <allison-higgins@bts.k12.nj.us>
2019-03-04 10:06:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/allison_higgins/beebuzz/BeeBuzzActivity.java
@@ -0,0 +1,13 @@
+package com.example.allison_higgins.beebuzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeeBuzzActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_bee_buzz);
+  }
+}
